4f8057859510b1d69b8b4b87ecc351827dfd26c7
Shari Meggs <smeggs1@gmail.com>
2016-08-29 16:46:12 -0700

    The first initialize test has been created and passes with the informaiton provided

--- specs/scoring_spec.rb
+++ specs/scoring_spec.rb
@@ -1,6 +1,13 @@
 require_relative 'spec_helper' # This is needed during the test
 require_relative '../scoring'
 
-describe Scoring do
-  
+describe Scrabble::Scoring do
+  describe "#initialize" do
+    tile = Scrabble::Scoring.new("letter")
+    it "This can create a new instance of Scoring" do
+      tile.must_be_instance_of(Scrabble::Scoring)
+    end
+
+
+  end
 end
