31516d3874c8983148a9fc51cc440d7231b1cd6d
Danielle Schrimmer <danielle.schrimmer@gmail.com>
2016-08-29 16:38:07 -0700

    Created initial module/class and data structure and added specs files.

---
+++ scoring.rb
@@ -0,0 +1,11 @@
+module Scrabble
+  class Scoring
+
+    LETTERS = {1 => %w(A E I O U L N R S T), 2 => %w(D G), 3 => %w(B C M P), 4 => %w(F H V W Y), 5 => ["K"], 8 => %w(J X), 10 => %w(Q Z) }
+
+    def initialize
+      @letters = {}
+    end
+
+  end
+end
