f6c0b2e0bab3d8d31786b91b2a78ee8454c28e9a
Suzannah Kirk-Daligcon <skirk.seattle@gmail.com>
2016-08-29 16:41:18 -0700

    Here is our first official red failed test.

--- specs/scrabble_spec.rb
+++ specs/scrabble_spec.rb
@@ -4,4 +4,10 @@ require_relative 'spec_helper'
 require_relative '../scoring'
 
 describe Scrabble::Scoring do
+    describe "#initialize" do
+        let(:score) {Scoring.new}
+        it "can create a new instance of Scoring" do
+            score.must_be_instance_of(Scoring)
+        end
+    end
 end
