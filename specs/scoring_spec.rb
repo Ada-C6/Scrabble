f3d0396e1dfe0d64706fdc8e955fbdcc3d111ee4
Shari Meggs <smeggs1@gmail.com>
2016-08-29 16:27:42 -0700

    Set up the specs to test out code. Scoring has the outline of the class

---
+++ specs/scoring_spec.rb
@@ -0,0 +1,6 @@
+require_relative 'spec_helper' # This is needed during the test
+require_relative '../scoring'
+
+describe Scoring do
+  
+end
---
+++ specs/spec_helper.rb
@@ -0,0 +1,7 @@
+require 'minitest'
+require 'minitest/spec'
+require "minitest/autorun"
+require "minitest/reporters"
+require 'minitest/pride'
+#These are all the things about mini test that we are using
+Minitest::Reporters.use! Minitest::Reporters::SpecReporter.new
