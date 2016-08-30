015621473b562d36a28a049a6bb53085821dacc9
Danielle Schrimmer <danielle.schrimmer@gmail.com>
2016-08-30 11:15:56 -0700

    Added a spec and score method appears to accurately return total score.

--- scoring.rb
+++ scoring.rb
@@ -4,7 +4,22 @@ module Scrabble
     LETTERS = {1 => %w(A E I O U L N R S T), 2 => %w(D G), 3 => %w(B C M P), 4 => %w(F H V W Y), 5 => ["K"], 8 => %w(J X), 10 => %w(Q Z) }
 
     def initialize
-      @letters = {}
+      @words = []
+    end
+
+    def self.score(word)
+      word.upcase!
+      current_score = 0
+      word_letters = word.split("")
+      word_letters.each do |i|
+        LETTERS.values.length.times do |j|
+          if LETTERS.values[j].include?(i)
+            current_score += LETTERS.keys[j]
+            break
+          end
+        end
+      end
+      return current_score
     end
 
   end
--- specs/scoring_spec.rb
+++ specs/scoring_spec.rb
@@ -3,13 +3,19 @@ require_relative '../scoring'
 
 describe Scrabble::Scoring do
 
-  describe "#score" do
-
-
-
+  describe "#scoring" do
 
     it "should return the total score value for the given word" do
       score.("dog").must_equal(5)
     end
+
+    a = Scrabble::Scoring.new
+
+    it "should throw an error when an invalid parameter is supplied" do
+      proc {a.score(123)}.must_raise(ArgumentError)
+    end
+
+    
+
   end
 end
