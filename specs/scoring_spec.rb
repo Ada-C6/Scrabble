f7745683cb012b3fc1aad1f559516e2166696197
Johna N. Morris <johna.n.morris@gmail.com>
2016-08-30 10:31:15 -0700

    renamed scrabble_spec.rb to scoring_spec.rb

--- specs/scoring_spec.rb
+++ specs/scoring_spec.rb
@@ -2,9 +2,9 @@ require_relative 'spec_helper'
 require_relative '../scrabble'
 
 describe Scrabble do
-#	describe Scoring do
+	describe Scrabble::Scoring do
 		it "should have a constant equal to a hash" do
 			Scrabble::Scoring::LETTERS.must_be_instance_of(Hash)
 		end
-#	end
+	end
 end
