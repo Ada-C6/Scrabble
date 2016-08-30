4d1091cb9cb6da09c7057fd48dde0949ef67eb47
Dianne Laguerta <diannelaguerta@gmail.com>
2016-08-30 11:15:07 -0700

    added 3 specs to Scoring and passed

--- specs/scoring_spec.rb
+++ specs/scoring_spec.rb
@@ -1,2 +1,28 @@
 require_relative "../lib/scoring.rb"
 require_relative 'spec_helper'
+module Scrabble
+describe Scoring do
+
+  describe "#initialize" do
+    let(:scoring) {Scoring.new(:word) }
+
+    it "can create an instance of Scoring" do
+      scoring.must_be_instance_of(Scoring)
+    end
+
+    it "has a word attribute" do
+    scoring.must_respond_to(:word)
+    end
+  end
+
+  describe "#self.score(word)" do
+  #let(:scoring) {Scoring.new("string") }
+
+    it "method will return a fixnum" do
+      Scoring.score("string").must_be_instance_of(Fixnum)
+    end
+
+  end
+
+end
+end
