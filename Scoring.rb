87ba82aed2fd9bfab086d53bd7a684f5346330de
Nicole Url <nicole.url.4@gmail.com>
2016-08-30 15:23:01 -0700

    WiP - Before lunch

--- Scoring.rb
+++ Scoring.rb
@@ -4,13 +4,13 @@ module Scrabble
     attr_reader
 
     SCORE_CHART = {
-      1=> ["A", "E", "I", "O", "U", "L", "N", "R", "S", "T"],
-      2=> ["D", "G"],
-      3=> ["B", "C", "M", "P"],
-      4=> ["F", "H", "V", "W", "Y"],
-      5=> ["K"],
-      8=> ["J", "X"],
-      10=> ["Q", "Z"]
+      1 => ["A", "E", "I", "O", "U", "L", "N", "R", "S", "T"],
+      2 => ["D", "G"],
+      3 => ["B", "C", "M", "P"],
+      4 => ["F", "H", "V", "W", "Y"],
+      5 => ["K"],
+      8 => ["J", "X"],
+      10 => ["Q", "Z"]
     }
 
     @array_of_words = []
@@ -21,14 +21,31 @@ module Scrabble
     end
 
     def collect_word(word)
-       #@array_of_words << word
+      # @array_of_words << word
     end
 
-    # def self.score
-    # end
-    #
-    # def self.highest_score_from_array(array_of_words)
-    # end
+    def self.score(word)
+      letter_array = word.upcase.split(//)
+      single_word_score = []
+
+      letter_array.each do |i|
+        Scrabble::Scoring::SCORE_CHART.each do |letter_score, letters|
+          if letters.include? i
+            single_word_score << key
+            break
+          end
+        end
+      end
+
+      if key's array .include? i, return key(i)
+
+
+
+      # single_word_score.inject(:+)
+    end
+
+    def self.highest_score_from_array(array_of_words)
+    end
 
   end
 end
--- specs/Scoring_spec.rb
+++ specs/Scoring_spec.rb
@@ -8,7 +8,7 @@ describe Scrabble::Scoring do
   end
 
   it "should have array_of_words" do
-      :array_of_words.wont_be_nil
+    :array_of_words.wont_be_nil
   end
 
   describe "#initialize" do
@@ -26,11 +26,18 @@ describe Scrabble::Scoring do
       Scrabble::Scoring.new("word").must_respond_to(:collect_word)
     end
 
+    # it "should pass in a string" do
+    #   (:word).must_be_kind_of(String)
+    # end
+
   end
 
   describe "#collect_word" do
 
-    it "should "
+    it "should add a word to the array" do
+      scrabble_scorecard = Scrabble::Scoring.new()
+      collect_word(flibble)
+    end
 
   end
 
