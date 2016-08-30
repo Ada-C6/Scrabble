422e7ea2167960557510c0acc03bcc4ecee302b1
Yuri Nakashima <yuri310@gmail.com>
2016-08-30 12:33:35 -0700

    4 tests completed

--- specs/scoring_spec.rb
+++ specs/scoring_spec.rb
@@ -10,4 +10,34 @@ describe Scrabble::Scoring do
 
 
   end
+
+  describe "scoring" do
+    it "can properly score the word 'cat'" do
+      Scrabble::Scoring.score("cat").must_equal(5) # 3,1,1
+    end
+
+    it "raises ArgumentError if string is empty" do
+      Scrabble::Scoring.score("").must_raise(ArgumentError)
+    end
+
+
+
+    it "raises ArgumentError if string is more than seven letters" do
+      if  word.length > 7
+        Scrabble::Scoring.score(word).must_raise(ArgumentError)
+      end
+    end
+
+
+  end
+
+  describe "highest_score_from" do
+    it "can return the highest scoring word from ["cat", "mat"]" do
+     Scrabble::Scoring.highest
+    end
+
+  end
+
+
+
 end
