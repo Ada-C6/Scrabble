0eb00dbec391c2f1e667d46e4a7585bb6ed96b32
Rachel <rachel.l.pavilanis@gmail.com>
2016-08-30 10:36:25 -0700

    Added test to scoring_spec.rb file

--- specs/scoring_spec.rb
+++ specs/scoring_spec.rb
@@ -1,1 +1,14 @@
 require_relative 'Spec_helper'
+
+
+require_relative '../lib/Scoring'
+
+describe Scoring do
+
+# may also be self.score(word) for method instead of .score(word)
+# The letters A, E, I, O, U, L, N, R, S, T equal 1 point.
+  it 'A converts to 1' do
+    expect( Scoring.new(:A).score(word) ).must_equal(1)
+  end
+
+end
--- specs/scrabble_spec.rb
+++
@@ -1,1 +0,0 @@
-require_relative 'Spec_helper'
