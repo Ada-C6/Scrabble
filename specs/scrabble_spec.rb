c01db0867b47658e03769370c9a8b75ffd4a785e
Lauren Brink <laurenfriesbrink@gmail.com>
2016-08-30 14:56:40 -0700

    trying to push spec updates again

--- specs/scrabble_spec.rb
+++ specs/scrabble_spec.rb
@@ -19,7 +19,20 @@ describe 'testing scrabble module' do
   end
 
   it 'score method must return correct score for a word' do
-    expect(Scoring.score("hello")).must_equal(8)
+    expect(Scrabble::Scoring.score("hello")).must_equal(8)
+  end
+
+  it 'score method must add 50 points to score for a 7-letter word' do
+    expect(Scrabble::Scoring.score("jazzmen").must_equal(84))
+  end
+
+  it 'score method should return a fixnum' do
+    expect(Scrabble::Scoring.score("something").must_be_instance_of(Fixnum))
+  end
+
+  it 'score method\'s argument should only include letters' do
+    expect( proc {Scrabble::Scoring.score("everything!!!")} ).must_raise(ArgumentError)
+    expect( proc {Scrabble::Scoring.score("everything123")} ).must_raise(ArgumentError)
   end
 
 end
