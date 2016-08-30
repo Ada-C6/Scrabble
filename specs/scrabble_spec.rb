0d25cd4bc256dea7a386b47d08ffbddd29348598
Sabrina Wilbert <sabrinawilbert@gmail.com>
2016-08-30 11:05:43 -0700

    added first test

--- specs/scrabble_spec.rb
+++ specs/scrabble_spec.rb
@@ -1,3 +1,17 @@
 require_relative 'spec_helper'
-require_relative '../scrabble.rb' # this is the module file
-require_relative '../lib/scrabble_scoring.rb'
+require_relative '../scrabble' # this is the module file
+require_relative '../lib/scrabble_scoring'
+
+describe 'testing scrabble module' do
+
+  it 'Must return correct key value' do
+    expect(Scrabble::SCORE_HASH[:A]).must_equal(1)
+    expect(Scrabble::SCORE_HASH[:D]).must_equal(2)
+    expect(Scrabble::SCORE_HASH[:B]).must_equal(3)
+    expect(Scrabble::SCORE_HASH[:F]).must_equal(4)
+    expect(Scrabble::SCORE_HASH[:K]).must_equal(5)
+    expect(Scrabble::SCORE_HASH[:X]).must_equal(8)
+    expect(Scrabble::SCORE_HASH[:Q]).must_equal(10)
+  end
+
+end
