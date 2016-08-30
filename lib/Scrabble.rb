91b85c25f8ed81a80603379a70735a6b9a4a43b8
Olivia <reach.olivialegge@gmail.com>
2016-08-30 15:06:07 -0700

    Three working tests, returns score

--- lib/Scrabble.rb
+++ lib/Scrabble.rb
@@ -5,28 +5,29 @@ require_relative '../Scrabble.rb'
 
 class Scrabble::Scoring
 
-  attr_reader :word, :points, :word_array, :letter_scores
-  @word_array = []
+  # attr_reader :word, :points, :word_array, :letter_scores
+  # @word_array = []
 
-  def self.letters
-    @letter_scores = {"A"=>1, "B"=>3, "C"=>3, "D"=>2,
+  LETTER_SCORES = {"A"=>1, "B"=>3, "C"=>3, "D"=>2,
    "E"=>1, "F"=>4, "G"=>2, "H"=>4,
    "I"=>1, "J"=>8, "K"=>5, "L"=>1,
    "M"=>3, "N"=>1, "O"=>1, "P"=>3,
    "Q"=>10, "R"=>1, "S"=>1, "T"=>1,
    "U"=>1, "V"=>4, "W"=>4, "X"=>8,
    "Y"=>4, "Z"=>10}
-  end
 
-  def self.score(word)
+   def self.score(word)
 
-      raise ArgumentError unless word.class == String
-      word.upcase
-        word.split(//).each do |char|
-          p char
-        end
-        print word.split(//)
-  end
+       raise ArgumentError unless word.class == String
+       @word = word.upcase
+       letters = @word.split(//)
+       score = 0
+       letters.each do |char|
+         score += LETTER_SCORES[char]
+       end
+       puts "\n#{@word} scores #{score} points\n\n"
+       return score
+   end
 
   def self.points
   end
--- specs/Scrabble_spec.rb
+++ specs/Scrabble_spec.rb
@@ -4,19 +4,18 @@ require_relative 'spec_helper.rb'
 describe "Testing Scrabble" do
 
   it "It must raise an ArgumentError if given a Fixnum" do
-      # expect(Scrabble::Scoring.score(word)).must_equal("HELLO")
+
        expect (proc { Scrabble::Scoring.score(11)} ).must_raise ArgumentError
-    #  expect (proc { Scrabble::Scrabble.score(Fixnum)} ).must_raise ArgumentError
 
   end
-  # it "Testing for cases divisible by 3 and not 5" do
-  #     expect(FizzBuzz::Fizzbuzz.fizzbuzz(3)).must_equal("Fizz")
-  #     expect(FizzBuzz::Fizzbuzz.fizzbuzz(6)).must_equal("Fizz")
-  #     expect(FizzBuzz::Fizzbuzz.fizzbuzz(9)).must_equal("Fizz")
+
+  # it "It must raise an ArgumentError if LETTER_SCORES[char] is not a Fixnum" do
+  #   expect (proc { Scrabble::Scoring.score("hellO")} ).must_raise ArgumentError
   # end
 
   it "Testing to remove case sensitivity" do
-      expect(Scrabble::Scoring.score("HeLLo")  ).must_equal("HELLO")
+
+      expect(Scrabble::Scoring.score("HeLLo")  ).must_equal(8)
   end
 
   it "Testing to make sure score is correct" do
