81abdbd4cd9e6a12a9f941b9c786c27dd2a66bc8
Yuri Nakashima <yuri310@gmail.com>
2016-08-30 12:32:49 -0700

    working on score method

--- scoring.rb
+++ scoring.rb
@@ -2,11 +2,24 @@ module Scrabble
   class Scoring
     attr_reader :letters
 
-    SCORE_CHART = { 1 => %w(A E I O U L N R S T), 2 => %w(D G), 3 => %w(B C M P), 4 => %w(F H V W Y), 5 => %w(k), 8 => %w(J X), 10 => %w(Q Z)}
+ SCORE_CHART = { "A" =>1, "E"=>1, "I"=> 1, "O"=> 1, "U"=> 1, "L"=> 1, "N"=> 1, "R" => 1, "S" => 1, "T" => 1,"D"=>2,"G"=>2,
+   "B" =>3, "C" => 3, "M" => 3, "P" => 3,"F"=>4, "H"=>4, "V"=>4, "W"=>4, "Y"=>4, "K"=>5, "J" =>8, "X"=>8, "Q"=>10, "Z" => 10 }
 
     def initialize (letters)
-      @letter = []
+      @letters = []
     end
 
+    def self.score(word)
+      score_array = []
+      word = "word"
+      word.each_char do |l|
+
+          score_array << SCORE_CHART.values_at(l)
+
+
+      end
+      return score_array
+      #.reduce(:+)
+    end
   end
 end
