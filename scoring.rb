ac42848e606fb71e4f13f08e9ec951f2e2b13204
Shari Meggs <smeggs1@gmail.com>
2016-08-29 17:01:44 -0700

    We added a hash to the program where the values are keys and letters are values

--- scoring.rb
+++ scoring.rb
@@ -1,9 +1,11 @@
 module Scrabble
   class Scoring
-    attr_reader :letter
+    attr_reader :letters
 
-    def initialize (letter)
-      @letter = letter
+    SCORE_CHART = { 1 => %w(A E I O U L N R S T), 2 => %w(D G), 3 => %w(B C M P), 4 => %w(F H V W Y), 5 => %w(k), 8 => %w(J X), 10 => %w(Q Z)}
+
+    def initialize (letters)
+      @letter = []
     end
 
   end
