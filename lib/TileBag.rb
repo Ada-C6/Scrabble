892eb71a0aa0a954ba4d539eaf1505e212492578
Trish Griego <pmgriego@gmail.com>
2016-08-31 15:10:10 -0700

    Started out creating the logic for default tile bag and then had to step away. Rachel took over. Instead of using hash we decided to go wtih an 2d array

--- lib/TileBag.rb
+++ lib/TileBag.rb
@@ -7,8 +7,34 @@ class Tilebag
   def initialize
 # lettters_array = []
 # called When you use TileBag.new sets up an instance with a collection of default tiles.
-default_tile_bag = 
-
+default_tile_bag = {
+"A" =>
+"B" =>
+"C" => 3,
+"D" =>
+"E" => 1
+"F" =>
+"G" =>
+"H" =>
+"I" =>
+"J" => 8
+"K"=> 5
+"L"=> 1
+"M"=> 3
+"N"=> 1
+"O"=> 1
+"P"=> 3
+"Q"=> 10
+"R"=> 1
+"S"=> 1
+"T"=> 1
+"U"=> 1
+"V"=> 4
+"W"=> 4
+"X"=> 8
+"Y"=> 4
+"Z"=> 10
+}
   end
 
   def draw_tiles(num)
