6171d316d53aa4111b339402e91ee9e1a744714b
Suzannah Kirk-Daligcon <skirk.seattle@gmail.com>
2016-08-31 15:30:21 -0700

    created a test to check for a play(word) method

--- player.rb
+++ player.rb
@@ -1,10 +1,21 @@
+require_relative 'scoring'
+
 module Scrabble
     class Player
-        attr_reader :name
-        
+        attr_reader :name, :plays
+
         def initialize(name)
             @name = name
+            @plays = []
+            @total_score = 0
         end
 
+        def play(word)
+            return Scoring.score(word)
+        end
+
+        def won?
+            return @total_score > 100
+        end
     end
 end
--- specs/player_spec.rb
+++ specs/player_spec.rb
@@ -2,15 +2,35 @@ require_relative 'spec_helper'
 require_relative '../player'
 
 describe Scrabble::Player do
+    let(:player_test) {Scrabble::Player.new("InuYasha")}
     describe "#initialize" do
-        let(:player_test) {Scrabble::Player.new("Sally")}
-
         it "can create a new instance of Player" do
             player_test.must_be_instance_of(Scrabble::Player)
         end
 
         it "has a name" do
-            Scrabble::Player.new("InuYasha").name.must_equal("InuYasha")
+            player_test.name.must_equal("InuYasha")
+        end
+
+        it "has an array containing words played" do
+            player_test.must_respond_to(:plays)
+        end
+    end
+
+    describe "#won?" do
+        it "returns true if total score is greater than 100" do
+            player_test.won?.must_equal(false)
+        end
+    end
+
+    describe "#play(word)" do
+        it "returns the score of a word before winning" do
+            player_test.play("check").must_equal(16)
+        end
+
+        it "will change the plays array" do
+            player_test.play("check")
+            player_test.plays.must_equal(["check"])
         end
     end
 end
