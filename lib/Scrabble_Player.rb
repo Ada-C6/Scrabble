94a8e757718cc49fc4d3a6a006efc314ec28302b
Lauren Brink <laurenfriesbrink@gmail.com>
2016-08-31 14:24:22 -0700

    added play(word) and tests for such

--- lib/Scrabble_Player.rb
+++ lib/Scrabble_Player.rb
@@ -1,9 +1,17 @@
 require_relative '../scrabble.rb'
 require_relative 'scrabble_scoring.rb'
 class Scrabble::Player
-  attr_reader :name
+  attr_reader :name, :plays
 
   def initialize(name)
     @name = name
+    @plays = []
   end
-end
+
+  def play(word)
+    @plays << word
+    score = Scrabble::Scoring.score(word)
+    return score
+  end
+
+end # end of class
--- specs/Player_spec.rb
+++ specs/Player_spec.rb
@@ -3,18 +3,27 @@ require_relative '../scrabble' # this is the module file
 require_relative '../lib/scrabble_scoring'
 require_relative '../lib/scrabble_player'
 
-player = Scrabble::Player.new("sabrina")
 describe 'testing player class' do
 
-
-
-  it 'returns the value of the @name instance variable' do
-    expect(player.name.must_equal("sabrina"))
+  before do
 
   end
 
+  it 'returns the value of the @name instance variable' do
+    player = Scrabble::Player.new("sabrina")
+    expect(player.name.must_equal("sabrina"))
+  end
 
+  it 'play(word) method returns the score of word' do
+    player = Scrabble::Player.new("sabrina2")
+    expect(player.play("zoo").must_equal(12))
+  end
 
+  it 'play(word) adds word to plays array' do
+    player = Scrabble::Player.new("sabrina3")
+    player.play("zoo")
+    expect(player.plays.must_include("zoo"))
+  end
 
   # it 'returns an Array of the words played by the player' do
   #   expect(Scrabble::Player.play(["sabrina", "cat", "Boxer"]).must_equal(["sabrina", "cat", "Boxer"]))
