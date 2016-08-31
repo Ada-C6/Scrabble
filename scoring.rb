67a74219e6ba9107ef9de5a7078690424144d814
Kate Shaffer <kate.shaffer@gmail.com>
2016-08-31 11:17:29 -0700

    Updated scoring.rb with Kari's feedback -- better ternary in bonus_score, renamed TILES constant hash, and updated highest_score_from logic. Added error check if highest_score_from is passed an empty array.

--- scoring.rb
+++ scoring.rb
@@ -2,7 +2,7 @@ module Scrabble
     class Scoring
         attr_reader
 
-        HASH ={
+        TILES ={
             "1" => %w(A E I O U L N R S T),
             "2" => %w(D G),
             "3" => %w(B C M P),
@@ -16,7 +16,8 @@ module Scrabble
         end
 
         def self.bonus(word)
-            word.length == 7 ? bonus_score = 50 : bonus_score = 0
+            # ternary -- assigns bonus_score 50 if length 7 or 0 if else
+            bonus_score = word.length == 7 ? 50 : 0
             return bonus_score
         end
 
@@ -31,7 +32,7 @@ module Scrabble
 
             arr.each do |i|
                 result = nil
-                HASH.each do |k, v|
+                TILES.each do |k, v|
                     if v.include?(i)
                         result = k.to_i
                         score += result
@@ -40,11 +41,10 @@ module Scrabble
             end
 
             return score + self.bonus(word)
-
         end # self.score method
 
         def self.highest_score_from(array_of_words)
-            if array_of_words.class != Array
+            if array_of_words.class != Array || array_of_words.length == 0
                 raise ArgumentError, "Invalid input"
             end
 
@@ -66,23 +66,27 @@ module Scrabble
             # retrieve the array value corresponding to the highest score key in the hash
             winning_array = score_result[highest_score]
 
-            # now we consider the length of words inside the winning array
-            # create a hash that store the length-words pairs
-            winning_hash = {}
-            winning_array.map do |word|
-                len = word.length
-                if winning_hash.keys.include?(len)
-                    winning_hash[len] += [word]
-                else
-                    winning_hash[len] = [word]
+            # check whether winning array has 1 word or >1 word
+            if winning_array.length > 1
+                # now we consider the length of words inside the winning array
+                # create a hash that store the length-words pairs
+                winning_hash = {}
+                winning_array.map do |word|
+                    len = word.length
+                    if winning_hash.keys.include?(len)
+                        winning_hash[len] += [word]
+                    else
+                        winning_hash[len] = [word]
+                    end
                 end
-            end
-
-            # first check which word has 7 letters, then check for the letter with the shortest length. 'first' method on array will ensure we retrieve the letter who comes first in the original array input.
-            if winning_hash.keys.include?(7)
-                winning_word = winning_hash[7].first
-            else
-                winning_word = winning_hash[winning_hash.keys.min].first
+                # first check which word has 7 letters, then check for the letter with the shortest length. 'first' method on array will ensure we retrieve the letter who comes first in the original array input.
+                if winning_hash.keys.include?(7)
+                    winning_word = winning_hash[7].first
+                else
+                    winning_word = winning_hash[winning_hash.keys.min].first
+                end
+            elsif winning_array.length == 1
+                winning_word = winning_array.first
             end
 
             return winning_word
