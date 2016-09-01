b7a8e28bf09c74263eb65d6901dfca0baa4c0967
Madeleine Johnson <mjohnson3038@gmail.com>
2016-08-31 17:11:55 -0700

    working play(word)

--- player.rb
+++ player.rb
@@ -2,16 +2,27 @@ require_relative 'scoring'
 
 module Scrabble
   class Player
-    attr_accessor :name
+    attr_accessor :name, :play_array, :word
 
     def initialize(name)
       @name = name
+      @play_array = []
+      @word = word
     end
 
     def plays
+
+      return @play_array
     end
 
     def play(word)
+
+      @play_array << word
+      if won?
+        return false
+      else
+        return Scrabble::Scoring.score(word)
+      end
     end
 
     def total_score
@@ -28,3 +39,11 @@ module Scrabble
 
   end
 end
+
+jm = Scrabble::Player.new("JM")
+puts jm.play("BEAR")
+puts jm.plays
+puts jm.play("CHEETAH")
+puts jm.play("RAT")
+puts jm.play("CAT")
+puts jm.plays
--- specs/player_spec.rb
+++ specs/player_spec.rb
@@ -1,75 +1,83 @@
 require_relative 'spec_helper'
 require_relative '../player'
+require_relative '../scoring'
 
 describe Scrabble::Player do
   describe "#initialize" do
     it "can initialize a new instance of Player" do
-      player = Scrabble::Player.new(name)
+      player = Scrabble::Player.new(:name)
       player.must_be_instance_of(Scrabble::Player)
     end
   end
 
   describe "#plays" do
     it "should have an array of played words" do
-      player = Scrabble::Player.new(name)
+      player = Scrabble::Player.new("Jen")
       player.plays.must_equal(["BEAR", "CHEETAH", "RAT", "CAT"])
     end
   end
 
   describe "#play(word)" do
     it "should put the word into the plays array" do
-      player = Scrabble::Player.new(name)
-      player.plays.must_include("BEAR")
+      player = Scrabble::Player.new("Jen")
+      player.play("DOG")
+      player.plays.must_include("DOG")
     end
 
-    # it "should return false if player has already won game" do
-    #   player = Scrabble::Player.new(name)
-    #
-    # end
+    it "should return false if player has already won game else return the score" do
+      player = Scrabble::Player.new("Jen")
+      if player.won?
+        player.play(word).must_equal(false)
+      else
+        Scrabble::Scoring.new
+        Scrabble::Scoring.score("BEAR").must_equal(6)
+      end
+    end
+  end
 
-    it "should return the score of the word" do
+  describe "#total_score" do
+    it "should return the sum of the scores of played words" do
+      Scrabble::Player.new("Jen")
+      ["BEAR", "CHEETAH", "RAT", "CAT"].must_equal(77)
+    end
+  end
+
+  describe "#won?" do
+    it "should display if the person has won the game or not" do
+      player = Scrabble::Player.new("Jen")
+      if player.total_score >= 100
+        player.won?.must_equal(true)
+      else
+        player.won?.must_equal(false)
+      end
+    end
+  end
+
+  describe "#highest_scoring_word" do
+    it "return the highest scoring played word by a player" do
       Scrabble::Scoring.new
-      Scrabble::Scoring.score("BEAR").must_equal(6)
+      # can we use the Scoring class' highest_score_from method?
+      player = Scrabble::Player.new("Jen")
+      player.highest_scoring_word.must_equal(Scrabble::Scoring.highest_score_from(player.plays))
     end
 
-    describe "#total_score" do
-      it "should return the sum of the scores of played words" do
-        Scrabble::Player.new(name)
-        ["BEAR", "CHEETAH", "RAT", "CAT"].must_equal(77)
-      end
+    it "should be a string" do
+      player = Scrabble::Player.new("Jen")
+      player.plays.highest_scoring_word.must_be_instance_of(String)
+    end
+  end
+
+  describe "#highest_word_score" do
+    it "should return the highest_scoring_word score" do
+      player = Scrabble::Player.new("Jen")
+      player.plays.highest_word_score.must_equal(63)
     end
 
-    describe "#won?" do
-      it "should display if the person has won the game" do
-        player = Scrabble::Player.new(name)
-        if player.total_score >= 100
-          player.won?.must_equal(true)
-        end
-      end
-
-      it "should display if the person has not won the game yet" do
-        player = Scrabble::Player.new(name)
-        if player.total_score < 100
-          player.won?.must_equal(false)
-        end
-      end
-
-      describe "#highest_scoring_word" do
-        it "return the highest scoring played word by a player" do
-          Scrabble::Scoring.new
-          # can we use the Scoring class' highest_score_from method?
-          player = Scrabble::Player.new(name)
-          player.plays.highest_scoring_word.must_equal("CHEETAH")
-        end
-      end
-
-      describe "#highest_word_score" do
-        it "should return the highest_scoring_word score" do
-          player = Scrabble::Player.new(name)
-          player.plays.highest_word_score.must_equal(63)
-        end
-      end
+    it "should be a fixnum" do
+      player = Scrabble::Player.new("Jen")
+      player.plays.highest_word_score.must_be_instance_of(Fixnum)
     end
 
   end
+
 end
