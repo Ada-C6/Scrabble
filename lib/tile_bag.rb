a7a00df7c0fdd89b3f7d30ed7a829003e91148df
Dianne Laguerta <diannelaguerta@gmail.com>
2016-09-02 12:04:34 -0700

    fixed spec for changing tile bag, now using a constant hash without an array. yay?

--- lib/tile_bag.rb
+++ lib/tile_bag.rb
@@ -1,34 +1,38 @@
 module Scrabble
   class TileBag
 
-    TILES_BAG = {
-      "A" => [1,9],
-      "E" => [1,12],
-      "I" => [1,9],
-      "O" => [1,8],
-      "U" => [1,4],
-      "L" => [1,4],
-      "N" => [1,6],
-      "R" => [1,6],
-      "S" => [1,4],
-      "T" => [1,6],
-      "D" => [2,4],
-      "G" => [2,3],
-      "B" => [3,2],
-      "C" => [3,2],
-      "M" => [3,2],
-      "P" => [3,2],
-      "F" => [4,2],
-      "H" => [4,2],
-      "V" => [4,2],
-      "W" => [4,2],
-      "Y" => [4,2],
-      "K" => [5,1],
-      "J" => [8,1],
-      "X" => [8,1],
-      "Q" => [10,1],
-      "Z" => [10,1]
-    } # Potential to merge with Scoring TILES later
+    TILES_BAG = {"A" => 9,
+    "E" => 12,
+    "I" => 1
+  }
+    # TILES_BAG = {
+    #   "A" => [1,9],
+    #   "E" => [1,12],
+    #   "I" => [1,9],
+    #   "O" => [1,8],
+    #   "U" => [1,4],
+    #   "L" => [1,4],
+    #   "N" => [1,6],
+    #   "R" => [1,6],
+    #   "S" => [1,4],
+    #   "T" => [1,6],
+    #   "D" => [2,4],
+    #   "G" => [2,3],
+    #   "B" => [3,2],
+    #   "C" => [3,2],
+    #   "M" => [3,2],
+    #   "P" => [3,2],
+    #   "F" => [4,2],
+    #   "H" => [4,2],
+    #   "V" => [4,2],
+    #   "W" => [4,2],
+    #   "Y" => [4,2],
+    #   "K" => [5,1],
+    #   "J" => [8,1],
+    #   "X" => [8,1],
+    #   "Q" => [10,1],
+    #   "Z" => [10,1]
+    # } # Potential to merge with Scoring TILES later
 
     attr_reader :game_tile_bag
 
@@ -38,13 +42,14 @@ module Scrabble
     end
 
     def draw_tiles(num)
+      # local_bag = @game_tile_bag.clone
       new_player_tiles = 0
-      available_player_tiles = @game_tile_bag.map { |key, value_array| key} # "A", "B", "C"
+      available_player_tiles = @game_tile_bag.map { |key, value_array| key} # =>["A", "B", "C"]
       new_player_tiles = available_player_tiles.sample(num) #array of drawn letters
       new_player_tiles.each do |letter|
-        @game_tile_bag[letter][1] -= 1
+        @game_tile_bag[letter] -= 1
       end
-      return new_player_tiles #array
+      return new_player_tiles #array of letters
     end #draw_tiles
 
 
--- specs/tile_bag_spec.rb
+++ specs/tile_bag_spec.rb
@@ -24,12 +24,17 @@ module Scrabble
       end
 
       it "should remove tiles from the game_tile_bag" do
-        current_letters = tile_bag.game_tile_bag.clone # cloned hash
+        current_letters = (tile_bag.game_tile_bag).clone # cloned hash
+        puts "current letters 1: #{current_letters}"
+          # puts "current letters 1: #{tile_bag.game_tile_bag.object_id}"
         test_tile_array = tile_bag.draw_tiles(2) #array of letters
+        puts test_tile_array
+        puts "current letters 2: #{current_letters}"
+
         test_tile_array.each do |tile_letter|
-          number_of_letters_available = tile_bag.game_tile_bag[tile_letter][1]
-          current_letters[tile_letter][1].wont_equal(number_of_letters_available)
-          # @todo OUR BEAUTIFUL CODE DOES NOT WORK, MAYBE TRY USE .COLLECT or .EACH after all
+          number_of_letters_available = tile_bag.game_tile_bag[tile_letter]
+          puts "Game tile bag 3: #{tile_bag.game_tile_bag}"
+          current_letters[tile_letter].wont_equal(number_of_letters_available)
         end
 
 
@@ -45,3 +50,12 @@ module Scrabble
 
   end
 end
+#
+# current_letters = d.game_tile_bag.clone # cloned hash
+# test_tile_array = d.draw_tiles(2) #array of letters
+# test_tile_array.each do |tile_letter|
+#   number_of_letters_available = d.game_tile_bag[tile_letter][1]
+#   # current_letters[tile_letter][1].wont_equal(number_of_letters_available)
+#   puts current_letters[tile_letter][1] == number_of_letters_available
+# end
+#   #
